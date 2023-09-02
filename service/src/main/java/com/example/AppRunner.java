c79a57433b540265229a03c1952b1dc7f4b5900f
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-09-02 12:20:26 +0300

    i

--- service/src/main/java/com/example/AppRunner.java
+++ service/src/main/java/com/example/AppRunner.java
@@ -7,6 +7,5 @@ public class AppRunner {
     public static void main(String[] args) {
         UserService userService = new UserService();
         System.out.println(userService.getUser(25L));
-        int a = 12;
     }
 }
