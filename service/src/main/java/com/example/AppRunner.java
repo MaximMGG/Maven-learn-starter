58592dfc09f00366dd6f7230c72271cbb47cb28c
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-09-02 12:24:39 +0300

    add some

--- service/src/main/java/com/example/AppRunner.java
+++ service/src/main/java/com/example/AppRunner.java
@@ -7,5 +7,6 @@ public class AppRunner {
     public static void main(String[] args) {
         UserService userService = new UserService();
         System.out.println(userService.getUser(25L));
+        System.out.println();
     }
 }
